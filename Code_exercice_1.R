942f0fbd6706fce6d2ebe27d84fd3f74fb969e06
Guillaume-slize <guillaumeslizewicz@Guillaumes-MacBook-Pro-2.local>
2016-08-09 09:15:25 +0200

    Exercice_1

---
+++ Code_exercice_1.R
@@ -0,0 +1,42 @@
+##EXERCICE 1
+
+install.packages("RCurl")
+library(RCurl)
+x <- getURL("https://raw.githubusercontent.com/kosukeimai/qss/master/INTRO/turnout.csv")
+y <- read.csv(text = x)
+
+library("rio")
+filepath ="https://raw.githubusercontent.com/kosukeimai/qss/master/INTRO/turnout.csv"
+df = import(filepath)
+summary(df)
+dim(df)
+(df$total*100)/(df$VAP+df$overseas)#turnout with VAP+overseas
+(df$total*100)/(df$VEP)#turnout with VEP
+
+df$ANES-((df$total*100)/(df$VAP+df$overseas))
+df$ANES-((df$total*100)/(df$VEP))
+
+df$ANES-((df$total*100)/(df$VAP+df$overseas-df$felons-df$noncit))
+
+(df$total*100)/(df$VAP+df$overseas)
+calculate_turnout_VAP=function(year){
+  turnout_VAP=(total*100)/(VAP+overseas)
+  return(turnout_VAP)
+}
+VAP_turnout=(df$total*100)/(df$VAP+df$overseas)
+VEP_turnout=(df$total*100)/(df$VEP)
+df["VAP_turnout"]=(df$total*100)/(df$VAP+df$overseas)
+df["VEP_turnout"]=(df$total*100)/(df$VEP)
+
+df$VAP_turnout[seq(from=1, to=7, by=1)]
+
+mean(df$VAP_turnout[seq(from=1, to=7, by=1)])
+mean(df$VAP_turnout[seq(from=8, to=14, by=1)])
+df$VAP_turnout[seq(from=1, to=14, by=2)]
+
+mean(df$VAP_turnout[seq(from=1, to=14, by=2)])
+mean(df$VAP_turnout[seq(from=2, to=14, by=2)])
+mean(df$ANES[seq(from=1, to=14, by=2)])
+mean(df$ANES[seq(from=2, to=14, by=2)])
+
+mean(df$ANES-((df$total*100)/(df$VEP)))
