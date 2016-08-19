b591339cc232bb41fc60b0635555e97d6f7ec1d6
basgpol <basg@outlook.com>
2016-08-19 16:47:51 +0200

    add RMSE function

--- Exam_project/prediction_code.R
+++ Exam_project/prediction_code.R
@@ -18,7 +18,11 @@
 
 
 
-##================ 4.2 XXXXX  ================
+##================ 4.2 Create evaluation function  ================
+## creating a function that calculate the RMSE
+get.rmse = function(real, estimate){
+  return(sqrt(mean((real-estimate)^2)))
+}
 
 ##================ 4.3 XXXXX  ================
 
