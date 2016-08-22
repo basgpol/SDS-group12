b54cfa6757a60f69af5a42db583db813ce2ad89d
Guillaume-slize <guillaumeslizewicz@Guillaumes-MacBook-Pro-2.local>
2016-08-22 13:36:51 +0200

    cleaner prediction code without search

--- Exam_project/prediction_code.R
+++ Exam_project/prediction_code.R
@@ -2,7 +2,7 @@
 ##--------------------------------- 4. Prediction Models _---------------------------------
 ##=========================================================================================
 
-install.packages("glmnet")
+#install.packages("glmnet")
 library("glmnet")
 
 ## Notes ##
@@ -37,7 +37,9 @@ transfer.data = read.csv("https://raw.githubusercontent.com/basgpol/SDS-group12/
 predicting.var = c("transfer.fee", "positions", #"nationality", 
                    "appearances", "total.goals", "total.assists", 
                "minutes.pr.goal", "total.minutes.played", "contract.left.month", "transferage",
-               "league", "Status", "searchresults")
+               "league", "Status", 
+               #"searchresults",
+               "Status")
 
 
 
@@ -46,7 +48,7 @@ predicting.var = c("transfer.fee", "positions", #"nationality",
 ## Creating a vector with the count of 70 pct. of the sample size  
 train_size = floor(0.70 * nrow(transfer.data)) # creates a vector 
 
-## setting seed to inable reproductivity 
+## setting seed to enable reproductivity 
 set.seed(123)
 
 ## creating a vector with random numbers (count = tran_size)
@@ -66,11 +68,34 @@ get.rmse = function(real, estimate){
 
 ##================ 4.3 Baseline model: Simple average from training sample  ================
 estimate_M1 = mean(train_sample$transfer.fee) #calculating estimate from model 1
+
 get.rmse(test_sample$transfer.fee, estimate_M1) # calculating RMSE from estimate on test sample 
+
+#create new data frame
+train_sample.1<- train_sample %>% 
+  select(transfer.fee,league) 
+train_sample.1<- train_sample.1%>% 
+  mutate(index=1:487)
+
+#creating GGplot for visualisation
+p = ggplot(train_sample.1, aes(x = index , y = transfer.fee))+
+  geom_segment(aes(x= index, xend=index, y=transfer.fee, yend=estimate_M1), color="red") +
+  geom_point(aes(x = index, y = transfer.fee, color = "black"))   +
+  geom_line(aes(x = index, y = estimate_M1), color="green", size =1)+
+  theme(axis.title.x=element_blank(),
+        axis.text.x =element_blank(),
+        axis.ticks= element_line(color=NA),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.background = element_blank(),
+        text=element_text(family="Goudy Old Style"))
+
+gg <- ggplotly(p)  #using plotly to make it interactive
+gg
 
 
 ##================ 4.4 Ordinary least square model   ================
-Model_2 = lm(transfer.fee ~ ., data = (train_sample) # generating linear model on training data
+Model_2 = lm(transfer.fee ~ ., data = (train_sample)) # generating linear model on training data
 summary(Model_2)
 
 estimate_M2 = predict(Model_2, test_sample)
@@ -78,8 +103,9 @@ estimate_M2 = predict(Model_2, test_sample)
 get.rmse(test_sample$transfer.fee, estimate_M2)
 
 
-
-Model_2 = lm(train_sample$transfer.fee~ train_sample$positions+train_sample$Status+train_sample$transferage+train_sample$searchresults+train_sample$league) # generating linear model on training data
+Model_2 = lm(train_sample$transfer.fee~ train_sample$positions+train_sample$Status+train_sample$transferage+
+               #train_sample$searchresults+
+               train_sample$league) # generating linear model on training data
              summary(Model_2)
              
 estimate_M2 = predict(Model_2, test_sample)
@@ -88,43 +114,18 @@ estimate_M2
 get.rmse(test_sample$transfer.fee, estimate_M2)
 
 
-<<<<<<< HEAD
-=======
-get.rmse(test_sample$transfer.fee, estimate_M1)
->>>>>>> origin/master
 
-
-<<<<<<< HEAD
-
-=======
-#create new data frame
-train_sample.1<- train_sample %>% 
-  select(name,transfer.fee,Status,club.to) 
-train_sample.1<- train_sample.1%>% 
-  mutate(index=1:496)
-
-#creating GGplot
-p = ggplot(train_sample.1, aes(x = index , y = transfer.fee))+
-  geom_segment(aes(x= index, xend=index, y=transfer.fee, yend=estimate_M1), color="red") +
-  geom_point(aes(x = index, y = transfer.fee,text = paste(name, " to ", club.to)), color = "black")   +
-  geom_line(aes(x = index, y = estimate_M1), color="green", size =1)+
-  theme(axis.title.x=element_blank(),
-         axis.text.x =element_blank(),
-         axis.ticks= element_line(color=NA),
-         panel.grid.major = element_blank(),
-         panel.grid.minor = element_blank(),
-         panel.background = element_blank(),
-         text=element_text(family="Goudy Old Style"))
-
-gg <- ggplotly(p)  #using plotly to make it interactive
-gg
-<<<<<<< HEAD
 ##================ 4.5 Lasso model  ================
 ## Creating matrices with all regressors beacuse the glmnet function only works with matrices
 RegressorMatrix_train=model.matrix(~ positions+transferage+
-                                    league+Status+searchresults, train_sample)
+                                    league+
+                                     #searchresults+
+                                   Status, train_sample)
+
 RegressorMatrix_test=model.matrix(~ positions+transferage+
-                                    league+Status+searchresults, test_sample)
+                                    league+
+                                    #searchresults+
+                                    Status, test_sample)
 
 
 ## Training Lasso
@@ -132,11 +133,11 @@ Model_3 = glmnet(x = RegressorMatrix_train, y = train_sample$transfer.fee)
 Model_3
 
 ##
-estimate_M3 = predict(Model_3, RegressorMatrix_test)
-estimate_M3
+# estimate_M3 = predict(Model_3, RegressorMatrix_test)
+# estimate_M3
 
 
-get.rmse(test_sample$transfer.fee, estimate_M3)
+# get.rmse(test_sample$transfer.fee, estimate_M3)
 
 # Calculating RSME for each lambda
 lambda_values = Model_3$lambda
@@ -161,34 +162,24 @@ ggplot(performance_Lasso, aes(x = lambda, y = RMSError))+
 best.lambda = performance_Lasso$lambda[performance_Lasso$RMSError == min(performance_Lasso$RMSError)]
 
 ## Coefficients for best models
-coef(M3_Lasso, s = best.lambda)
+coef(Model_3, s = best.lambda)
 
 ## RMSE for best model
 get.rmse(predict(Model_3, RegressorMatrix_test, s=best.lambda), test_sample$transfer.fee)
 
 
 ##================ 4.6 Decision tree   ================
-#install.packages("rpart")
+# install.packages("rpart")
 library("rpart")
 Model_4=rpart(formula=transfer.fee~positions+transferage+
-                league+Status+searchresults+contract.left.month+total.goals+appearances, data=train_sample)
+                league+Status+
+                #searchresults+
+                contract.left.month+total.goals+appearances, data=train_sample)
 
 printcp(Model_4)
+summary(Model_4)
 
 
-##=====================================================
 
-#adding theme
-gg+theme(axis.title.x=element_blank(),
-         axis.text.x =element_text(size  = 7,
-                                   angle = 45,
-                                   hjust = 1,
-                                   vjust = 1),
-         axis.ticks= element_line(color=NA),
-         panel.grid.major = element_blank(),
-         panel.grid.minor = element_blank(),
-         panel.background = element_blank(),
-         axis.title.y=element_blank(),
-         text=element_text(family="Goudy Old Style"))
 ##================ 4.6   ================>>>>>>> origin/master
 
